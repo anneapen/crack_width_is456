8eb3ef07ae2bd0e2e08eaa888403a3cf32fb15df
anneapen <annmary181@gmail.com>
2025-12-03 17:22:03 +0530

    Added concrete_stress_comp() & test_concrete_stress_comp()

--- cw_calc.py
+++ cw_calc.py
@@ -1,3 +1,4 @@
+import pandas as pd
 
 #Determining acr
 def acr(s:float,dc:float,db)->float:
@@ -7,3 +8,14 @@ def acr(s:float,dc:float,db)->float:
 
     acr=((((s/2)**2)+(dc**2))**(1/2))-(db/2)
     return round(acr,2)
+
+#Determining the concrete stress in compression
+def concrete_stress_comp(fck:int):
+    """
+    Returns the compressive stress in concrete
+    """
+    cbc_df=pd.read_csv("Concrete stress in compression.csv")
+    cbc_df=cbc_df[:10]
+    cbc_df=cbc_df.set_index("fck")
+    cbc=cbc_df.loc[fck,'cbc']
+    return cbc
--- test_cw_calc.py
+++ test_cw_calc.py
@@ -3,3 +3,7 @@ import cw_calc as cw_calc
 def test_acr():
     acr=cw_calc.acr(s=450,dc=25,db=10)
     assert acr==221.38
+
+def test_concrete_stress_comp():
+    cbc=cw_calc.concrete_stress_comp(fck=30)
+    assert cbc==10.0
