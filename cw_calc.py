055878259d4961d7966aa2dfcdf87303d42e7214
anneapen <annmary181@gmail.com>
2025-12-04 19:09:12 +0530

    Added crack_width() & test_crack_width()
    Added the function to calculate the crack width and the test function. Modified acr() & test to concrete_area()

--- cw_calc.py
+++ cw_calc.py
@@ -1,7 +1,8 @@
 import pandas as pd
+import math
 
 #Determining acr
-def acr(s:float,dc:float,db)->float:
+def concrete_area(s:float,dc:float,db)->float:
     """
     Calculating acr
     """
@@ -19,3 +20,25 @@ def concrete_stress_comp(fck:int):
     cbc_df=cbc_df.set_index("fck")
     cbc=cbc_df.loc[fck,'cbc']
     return cbc
+
+def crack_width(fck:float,b:float,D:float,dc:float,Cmin:float,s:float,db:float,M:float)->float:
+    """
+    To determine the crack width
+    """
+    Es=200000
+    d=D-dc
+    cbc=concrete_stress_comp(fck)
+    m=280/(3*cbc)
+    Ast=(1000/s)*(math.pi/4*(db**2))
+    pt=(Ast/(b*d))*100
+    rho=pt/100
+    k=math.sqrt((2*rho*m)+((rho*m)**2))-(rho*m)
+    x=k*d
+    Icr=((b*x**3/3)+((m*Ast)*((d-x)**2)))/(10**8)
+    fst=((m*(M*10**6)*(d-x))/Icr)/(10**8)
+    e1=(fst/Es)*((D-x)/(d-x))
+    e2=(b*(D-x)**2)/(3*Es*Ast*(d-x))
+    em=e1-e2
+    acr=concrete_area(s,dc,db)
+    wcr=(3*acr*em)/(1+(2*(acr-Cmin))/(d-x))
+    return round(wcr,2)
--- test_cw_calc.py
+++ test_cw_calc.py
@@ -1,9 +1,13 @@
 import cw_calc as cw_calc
 
-def test_acr():
-    acr=cw_calc.acr(s=450,dc=25,db=10)
+def test_concrete_area():
+    acr=cw_calc.concrete_area(s=450,dc=25,db=10)
     assert acr==221.38
 
 def test_concrete_stress_comp():
     cbc=cw_calc.concrete_stress_comp(fck=30)
     assert cbc==10.0
+
+def test_crack_width():
+    wcr=cw_calc.crack_width(30,160,850,25,25,450,10,45)
+    assert wcr==0.23
