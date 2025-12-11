d34e98ce92549761d34f98426615a954603867a6
anneapen <annmary181@gmail.com>
2025-12-11 12:18:09 +0530

    Updated cw_app.py

--- cw_app.py
+++ cw_app.py
@@ -12,7 +12,7 @@ dc=st.number_input("Clear cover,dc(mm)",min_value=0.0)
 db=st.number_input("Diameter of the bar,db(mm)",min_value=0.0)
 s=st.number_input("Spacing between bars, s(mm)",min_value=0.0)
 Cmin=st.number_input("Minimum cover to the main bar, Cmin(mm)",min_value=0.0)
-M=st.number_input("Maximum Moment(kNm)",min_value=0.0)
+M=st.number_input("Maximum Moment at midspan(kNm)",min_value=0.0)
 
 wcr=cw_calc.crack_width(fck,b,D,dc,Cmin,s,db,M)
 
